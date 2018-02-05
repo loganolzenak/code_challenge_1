0f5713dff2f60ed22d799d9ef8caed60b68313bc
Logan Olzenak <logan.olzenak@gmail.com>
2018-02-05 15:10:40 -0600

    code challenge

---
+++ scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $('#generator').on('click', function () {
+
+  $('div').append('<p></p>');
+
+    console.log("on click");
+    // shows clicks in console, cant get it to the dom
+  }
+);});
